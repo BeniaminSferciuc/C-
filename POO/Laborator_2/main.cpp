c4bdf064a5c8ece4c486295d3fdea2595a3aceab
Beniamin Sferciuc <sbt.beniamin@gmail.com>
2023-11-06 13:07:43 +0200

    Update the algoritm (days between two given dates)

--- POO/Laborator_2/main.cpp
+++ POO/Laborator_2/main.cpp
@@ -1,58 +1,60 @@
 #include <iostream>
-#include <iomanip>
 
-using namespace std;
+class Date {
+private:
+    int day, month, year;
 
-    class Data {
-        int _an, _luna, _zi; // sunt date membre
+public:
+    Date(int d, int m, int y) : day(d), month(m), year(y) {}
 
-    public:
-        void SetData( int an, int luna, int zi);
-        void Print();
-        int NrZile(Data dc);
-    };
-
-    string luni[] = { "", "IANUARIE", "FEBRUARIE", "MARTIE", "APRILIE",
-                    "MAI","IUNIE","IULIE","AUGUST","SEPTEMBRIE",
-                    "OCTOMBRIE", "NOIEMBRIE","DECEMBRIE"
-    };
-
-    int nr_zile_luni[] = {0,31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    void Data::SetData( int an, int luna, int zi) {
-        _an = an;
-        _luna = luna;
-        _zi = zi;
+    int getDay() const {
+        return day;
     }
 
-
-    void Data::Print() {
-        cout << endl << setw(4) << _an << setw(12) << luni[_luna] << setw(3) << _zi;
+    int getMonth() const {
+        return month;
     }
 
-    int Data::NrZile(Data dc)
-    {
-        int nr = 0;
-
-        if (_an % 4 == 0)
-
-        cout << endl << "prima: " << dc._luna << endl;
-        cout << endl << "second: " << _luna << endl;
-
-        nr += dc._zi >= _zi ? dc._zi - _zi : (dc._luna--, nr_zile_luni[_luna] + dc._zi - _zi);
-        nr += 30 * (dc._luna >= _luna ? dc._luna - _luna : (dc._an--, 12 + dc._luna - _luna));
-        nr += 365 * (dc._an - _an);
-
-        return nr;
+    int getYear() const {
+        return year;
     }
+};
 
-    int main() {
-        Data dn, dc; //obiecte apartinand clasei Data
-        dn.SetData(2024, 1, 1);
-        dc.SetData(2024, 3, 30);
-        dn.Print();
-        dc.Print();
-        cout << endl << "Nr zile trecute = " << dn.NrZile(dc);
+class DateCalculator {
+public:
+    static int daysBetweenDates(const Date& date1, const Date& date2) {
+        // Algoritmul pentru calculul numărului de zile
+        int daysInMonth[] = { 31, 28 + (date2.getYear() % 4 == 0 && (date2.getYear() % 100 != 0 || date2.getYear() % 400 == 0)), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-        return 0;
+        int days = 0;
+
+        // Adăugăm zilele pentru fiecare an între cele două date
+        for (int i = date1.getYear(); i < date2.getYear(); ++i) {
+            days += 365 + (i % 4 == 0 && (i % 100 != 0 || i % 400 == 0));
+        }
+
+        // Scădem zilele trecute din anul date1
+        for (int i = 0; i < date1.getMonth() - 1; ++i) {
+            days -= daysInMonth[i];
+        }
+        days -= date1.getDay();
+
+        // Adăugăm zilele din anul date2
+        for (int i = 0; i < date2.getMonth() - 1; ++i) {
+            days += daysInMonth[i];
+        }
+        days += date2.getDay();
+
+        return days;
     }
+};
+
+int main() {
+    Date date1(1, 2, 2024); // Prima dată
+    Date date2(1, 3, 2024); // A doua dată
+
+    int days = DateCalculator::daysBetweenDates(date1, date2);
+    std::cout << "Numarul de zile intre date: " << days << std::endl;
+
+    return 0;
+}
